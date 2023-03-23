b36473a0728718ea2677b7fed7aa0f33e480baee
Maijs Garais <maireadsbooks@gmail.com>
2023-03-23 12:32:11 +0000

    add skills section

---
+++ src/assets/skills.js
@@ -0,0 +1,13 @@
+const skills = [
+  { title: "HTML", years: 0 },
+  { title: "CSS", years: 0 },
+  { title: "SCSS", years: 0 },
+  { title: "JavaScript", years: 0 },
+  { title: "React", years: 0 },
+  { title: "Node", years: 0 },
+  { title: "Express", years: 0 },
+  { title: "Bootstrap", years: 0 },
+  { title: "Tailwind", years: 0 },
+];
+
+export default skills;
